246f1c0873027db02c1e30c6a6604b8ac7935ee3
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-09 22:27:34 +0500

    LINQ

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> list = new List<string>();
+list.Add("HONDA");
+list.Add("TOYOTA");
+list.Add("FERRARI");
+list.Add("LAMBORGHINI");
+list.Add("CHEVERLOT");
+list.Add("NISAAN");
+list.Add("TESLA");
+
+var query = from x in list where x.Contains('A') select x;
+foreach(var item in query)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("-------------------------------");
+list.Where(x => x.Contains('A')).Select(x=>x.Substring(0,3)).OrderBy(x=>x).ToList().ForEach(x=>Console.WriteLine(x));
