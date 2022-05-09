8aee35a01c1ad32de87524be8a08c0b9c3fb52aa
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-09 20:29:58 +0500

    initial commit

---
+++ MidTermN-Tier/MidTermN-TierBO/WeatherBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTermN_TierBO
+{
+    public class WeatherBO
+    {
+        public int Temperature { get; set; }
+        public int Precipitation { get; set; }
+        public int Wind { get; set; }
+        public string Location { get; set; }
+    }
+}
